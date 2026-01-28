f9502d9992fabc8a6ffc43502c5f9ff61afb0524
Jared Prather <jared.prather@chartspan.com>
2026-01-28 12:02:21 -0600

    feat: Removes json credentials

--- src/Alethic.Auth0.Operator/Models/V1Client.cs
+++ src/Alethic.Auth0.Operator/Models/V1Client.cs
@@ -32,7 +32,7 @@ namespace Alethic.Auth0.Operator.Models
             public V1TenantReference? TenantRef { get; set; }
 
             [JsonPropertyName("secretRef")]
-            public V1ClientSecretReference? SecretRef { get; set; }
+            public V1SecretReference? SecretRef { get; set; }
 
             [JsonPropertyName("find")]
             public ClientFind? Find { get; set; }
